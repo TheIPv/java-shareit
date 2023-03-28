42bf697dd8d4aa85dfb1db325e9f62394a033879
TheIPv <anymaxi@yandex.ru>
2023-03-28 13:05:04 +0300

    feat: add more tests

--- src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -10,8 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,7 +41,7 @@ public class ItemControllerTest {
         itemToAdd.setAvailable(true);
 
         itemController.addItem(1L, itemToAdd);
-        when(itemController.addItem(1L, itemToAdd))
+        when(itemController.addItem(anyLong(), any()))
                 .thenReturn(itemToAdd);
 
         mvc.perform(post("/items")
@@ -56,19 +60,10 @@ public class ItemControllerTest {
         itemToAdd.setAvailable(true);
 
         itemController.addItem(1L, itemToAdd);
-        when(itemController.addItem(1L, itemToAdd))
-                .thenReturn(itemToAdd);
-
-
-        mvc.perform(post("/items")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(gson.toJson(itemToAdd))
-                        .header("X-Sharer-User-Id", "1"))
-                .andExpect(status().isOk());
 
         itemToAdd.setName("updatedName");
         itemController.updateItem(1L, itemToAdd, 1L);
-        when(itemController.updateItem(1L, itemToAdd, 1L))
+        when(itemController.updateItem(anyLong(), any(), anyLong()))
                 .thenReturn(itemToAdd);
 
         mvc.perform(MockMvcRequestBuilders.patch("/items/1")
@@ -85,24 +80,60 @@ public class ItemControllerTest {
         itemToAdd.setName("item1");
         itemToAdd.setDescription("desc 1");
         itemToAdd.setAvailable(true);
-
         itemController.addItem(1L, itemToAdd);
-        when(itemController.addItem(1L, itemToAdd))
-                .thenReturn(itemToAdd);
-
-        mvc.perform(post("/items")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(gson.toJson(itemToAdd))
-                        .header("X-Sharer-User-Id", "1"))
-                .andExpect(status().isOk());
 
         itemController.getItemById(itemToAdd.getId(), 1L);
-        when(itemController.getItemById(itemToAdd.getId(), 1L))
+        when(itemController.getItemById(anyLong(), anyLong()))
                 .thenReturn(itemToAdd);
 
         mvc.perform(MockMvcRequestBuilders.get("/items/1")
                         .header("X-Sharer-User-Id", "1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUserItems() throws Exception {
+        ItemDto itemToAdd = new ItemDto();
+        itemToAdd.setName("item1");
+        itemToAdd.setDescription("desc 1");
+        itemToAdd.setAvailable(true);
+        itemController.addItem(1L, itemToAdd);
+
+        itemController.getUserItems(1L, 0, 1);
+        when(itemController.getUserItems(anyLong(), anyInt(), anyInt()))
+                .thenReturn(List.of(itemToAdd));
+
+        mvc.perform(MockMvcRequestBuilders.get("/items/")
+                        .header("X-Sharer-User-Id", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(gson.toJson(List.of(itemToAdd))));
+    }
+
+    @Test
+    public void searchItemByText() throws Exception {
+        itemController.searchItemByText("search", 0, 1);
+        when(itemController.searchItemByText(anyString(), anyInt(), anyInt()))
+                .thenReturn(List.of());
+
+        mvc.perform(MockMvcRequestBuilders.get("/items/search?text=search")
+                        .header("X-Sharer-User-Id", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(gson.toJson(List.of())));
+    }
+
+    @Test
+    public void createComment() throws Exception {
+        CommentDto commentDto = new CommentDto();
+        itemController.createComment(commentDto, 1L, 1L);
+        when(itemController.createComment(any(), anyLong(), anyLong()))
+                .thenReturn(commentDto);
+
+        mvc.perform(MockMvcRequestBuilders.post("/items/1/comment")
+                        .header("X-Sharer-User-Id", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
