43242e42fc06e6c0a93549703bb765a2d3286ac5
TheIPv <anymaxi@yandex.ru>
2023-05-03 17:47:22 +0300

    feat: refactor for sprint 16

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItGateway {
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItGateway.class, args);
-		System.out.println("*".repeat(134) + "\n" + "*".repeat(40) + "       Спринт №17. Add-Docker. Сервер Gateway запущен.      "
-				+ "*".repeat(40) + "\n" + "*".repeat(134));
 	}
 }
